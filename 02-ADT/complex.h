2ec72b8ecc6b805e5d897ee77fea992a353776ac
Ravi Kiran <ravika@gmail.com>
2021-02-15 12:52:53 +0530

    Changed function names, added new functions

--- 02-ADT/complex.h
+++ 02-ADT/complex.h
@@ -9,14 +9,14 @@ struct complex2_ {
     double b; // imaginary
 };
 
-complex2 CxAdd2(complex2 c1,complex2 c2); // complex addition
-complex2 CxMul2(complex2 c1,complex2 c2); // complex multiplication
-complex2 CxInv2(complex2 cx); // complex reciprocal 
-complex2 CxConj(complex2 cx); // complex conjugate
+complex2 Cx2Create(double real,double imag);
+double Cx2Arg(const complex2 cx); // theta
+double Cx2Mod(const complex2 cx); // |cx|
+void Cx2Print(const complex2 cx); // print the complex number
 
-double CxArg2(complex2 cx); // theta
-double CxMod2(complex2 cx); // |cx|
-
-void CxPrint2(complex2 cx); // print the complex number
+complex2 Cx2Add(const complex2 c1,const complex2 c2); // complex addition
+complex2 Cx2Mul(const complex2 c1,const complex2 c2); // complex multiplication
+complex2 Cx2Inv(const complex2 cx); // complex reciprocal 
+complex2 Cx2Conj(const complex2 cx); // complex conjugate
 
 #endif
