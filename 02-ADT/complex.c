5290c621e677c28a53000e141a1c98fd4edb3869
Ravi Kiran <ravika@gmail.com>
2021-02-22 15:53:23 +0530

    Added complex.h and sample files

---
+++ 02-ADT/complex.c
@@ -0,0 +1,8 @@
+#include "complex.h" // user-defined header files
+
+// Implementation of the ADT goes below ....
+
+double CxArg2(complex2 c1)
+{
+   
+}
---
+++ 02-ADT/main.c
@@ -0,0 +1,8 @@
+#include "complex.h"
+
+int main()
+{
+    complex2 c1,c2;
+
+    return 0;
+}
